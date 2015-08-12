66283ea77828b5c7d4fe74c113f55a639d57ec74
acatxnamedvirtue <tsb252@gmail.com>
2015-08-12 14:39:31 -0400

    Create ShortenedUrl model and migration, Create User model and migration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URLShortener::Application.config.secret_key_base = '09aba95cd544199554470e3b63d20d600f1ad5cfe6d1b7ae5d09055384792038b10270715edb9e9bf88badbc983f94c2a0dc1ab1eda8411499c7b08bbbe22e0b'
